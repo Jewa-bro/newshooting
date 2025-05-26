b895819c2eeb93727356c656c9f76a848476db17
Jewa-bro <wlsdyd103@gmail.com>
2025-05-27 00:36:14 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'build' ? '/newshooting/' : '/';
-
-  return {
-    base: base,
-    plugins: [react()],
-    optimizeDeps: {
-      exclude: ['lucide-react'],
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 1000,
     },
-    server: {
-      watch: {
-        usePolling: true,
-        interval: 1000,
-      },
-      hmr: {
-        overlay: false
-      }
+    hmr: {
+      overlay: false
     }
-  };
+  }
 });
